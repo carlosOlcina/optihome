dac99c5a2d55c0d5d92fba058683653429813ffa
carlosOlcina <carlosolcina23@gmail.com>
2026-01-15 12:39:12 +0100

    feat(landing): implement landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing',  loadComponent: () => import('./pages/landing/landing-page.component').then(m => m.LandingPage)}
+];
