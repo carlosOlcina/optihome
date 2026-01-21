5ee4e707d0f9b34bf8970995e8fc155ad175b7be
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 18:02:57 +0100

    fix: bad config in prettier for HTML files

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const prettier = require("eslint-plugin-prettier/recommended");
+const eslint = require('@eslint/js')
+const { defineConfig } = require('eslint/config')
+const tseslint = require('typescript-eslint')
+const angular = require('angular-eslint')
+const prettier = require('eslint-plugin-prettier/recommended')
 
 module.exports = defineConfig([
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
@@ -17,31 +17,29 @@ module.exports = defineConfig([
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: ["app", "landing", "shop", "ui"],
-          style: "kebab-case",
+          type: 'element',
+          prefix: ['app', 'landing', 'shop', 'ui'],
+          style: 'kebab-case',
         },
       ],
     },
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-      prettier,
-    ],
-    rules: {},
-  }
-]);
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility, prettier],
+    rules: {
+      'prettier/prettier': ['error', { parser: 'html' }],
+    },
+  },
+])
