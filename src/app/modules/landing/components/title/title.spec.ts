30abeea091d6972eb84fd63873540fb30b589020
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 16:10:41 +0100

    feat(landing): add `TitleLanding` component with styles, template, and unit test

---
+++ src/app/modules/landing/components/title/title.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TitleLanding } from './title';
+
+describe('Title', () => {
+  let component: TitleLanding;
+  let fixture: ComponentFixture<TitleLanding>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TitleLanding]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TitleLanding);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/landing/components/title/title.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-title',
+  imports: [],
+  templateUrl: './title.html',
+  styleUrl: './title.css',
+})
+export class TitleLanding {
+
+}
