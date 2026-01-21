ecf856226cc55c2a2f25cb2237f005c8f65227c9
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 09:06:35 +0100

    feat(landing-page): enhance benefits list with icons and centralized styling

--- src/app/pages/landing/landing-page.component.ts
+++ src/app/pages/landing/landing-page.component.ts
@@ -3,21 +3,29 @@ import {LandingMainSection} from '../../modules/landing/components/main-section/
 import {PrimaryButtonShared} from '../../share/components/buttons/primary-button/primary-button';
 import {Router} from '@angular/router';
 import {SizeEnum} from '../../config/enums/size-enum';
+import {LucideAngularModule, ShieldIcon, WifiIcon, ZapIcon} from 'lucide-angular';
+import {BrandColorsEnum} from '../../config/enums/colors/brand-colors-enum';
 
 @Component({
   selector: 'landing-page',
   imports: [
     LandingMainSection,
     PrimaryButtonShared,
+    LucideAngularModule,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
 export class LandingPage {
+  readonly shieldIcon = ShieldIcon
+  readonly zapIcon = ZapIcon
+  readonly wifiIcon = WifiIcon
+
   constructor(private router: Router) {}
 
   navigateToMain = ()=> {
     void this.router.navigate(['/shop']);
   }
   protected readonly SizeEnum = SizeEnum;
+  protected readonly BrandColorsEnum = BrandColorsEnum;
 }
