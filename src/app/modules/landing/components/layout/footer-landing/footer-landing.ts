182a5ccc8a7165f4fe1bb5e33db2d9eeebc3e04b
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 17:20:41 +0100

    feat(landing): add `FooterLanding` component with template, styles, and unit test

---
+++ src/app/modules/landing/components/layout/footer-landing/footer-landing.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Logo } from '../../../../../share/icons/logo/logo';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'landing-footer',
+  imports: [
+    Logo,
+    RouterLink
+  ],
+  templateUrl: './footer-landing.html',
+  styleUrl: './footer-landing.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterLanding {
+  readonly currentYear = signal(new Date().getFullYear());
+
+  readonly footerSections = signal([
+    {
+      title: 'Productos',
+      links: [
+        { label: 'Smart lights', href: '#' },
+        { label: 'Smart camera', href: '#' },
+        { label: 'Smart speaker', href: '#' },
+        { label: 'Climate hub', href: '#' }
+      ]
+    },
+    {
+      title: 'Empresa',
+      links: [
+        { label: 'Sobre nosotros', href: '#' },
+        { label: 'Contacto', href: '#' },
+        { label: 'Privacidad', href: '#' },
+        { label: 'Términos', href: '#' }
+      ]
+    },
+    {
+      title: 'Recursos',
+      links: [
+        { label: 'Documentación', href: '#' }
+      ]
+    }
+  ]);
+}
