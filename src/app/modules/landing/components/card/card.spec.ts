bb67550a4ba026fbead6803ba703e19c42118cff
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 16:58:10 +0100

    feat(landing): add `CardLanding` component with template, styles, and unit test

---
+++ src/app/modules/landing/components/card/card.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardLanding } from './card';
+
+describe('Card', () => {
+  let component: CardLanding;
+  let fixture: ComponentFixture<CardLanding>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CardLanding]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardLanding);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
