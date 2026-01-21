06f4f5de24d7e0cfd1ae8b959cdea628ecbfd5e6
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 16:59:36 +0100

    feat(landing-page): add "Why Local" section

--- src/app/pages/landing/landing-page.component.ts
+++ src/app/pages/landing/landing-page.component.ts
@@ -3,8 +3,19 @@ import {LandingMainSection} from '../../modules/landing/components/main-section/
 import {PrimaryButtonShared} from '../../share/components/buttons/primary-button/primary-button';
 import {Router} from '@angular/router';
 import {SizeEnum} from '../../config/enums/size-enum';
-import {LucideAngularModule, ShieldIcon, WifiIcon, ZapIcon} from 'lucide-angular';
+import {
+  DollarSignIcon,
+  LockIcon,
+  LucideAngularModule, ServerIcon,
+  ShieldIcon,
+  WifiIcon,
+  WifiOffIcon,
+  ZapIcon
+} from 'lucide-angular';
 import {BrandColorsEnum} from '../../config/enums/colors/brand-colors-enum';
+import {TitleLanding} from '../../modules/landing/components/title/title';
+import {TitleParagraphLanding} from '../../modules/landing/components/title-paragraph/title-paragraph';
+import {CardLanding} from '../../modules/landing/components/card/card';
 
 @Component({
   selector: 'landing-page',
@@ -12,6 +23,9 @@ import {BrandColorsEnum} from '../../config/enums/colors/brand-colors-enum';
     LandingMainSection,
     PrimaryButtonShared,
     LucideAngularModule,
+    TitleLanding,
+    TitleParagraphLanding,
+    CardLanding,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
@@ -20,6 +34,10 @@ export class LandingPage {
   readonly shieldIcon = ShieldIcon
   readonly zapIcon = ZapIcon
   readonly wifiIcon = WifiIcon
+  readonly wifiOffIcon = WifiOffIcon
+  readonly dolarIcon = DollarSignIcon
+  readonly lockIcon = LockIcon
+  readonly serverIcon = ServerIcon
 
   constructor(private router: Router) {}
 
