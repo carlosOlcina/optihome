83ffcffe1268a619ad39d87214495a812cd71b2d
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 17:21:11 +0100

    feat(landing): integrate `FooterLanding` into `LandingLayout`

--- src/app/modules/landing/layouts/landing-layout/landing-layout.ts
+++ src/app/modules/landing/layouts/landing-layout/landing-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet} from '@angular/router';
 import {HeaderLanding} from '../../components/layout/header-landing/header-landing';
+import {FooterLanding} from '../../components/layout/footer-landing/footer-landing';
 
 @Component({
   selector: 'landing-layout',
   imports: [
     RouterOutlet,
-    HeaderLanding
+    HeaderLanding,
+    FooterLanding
   ],
   templateUrl: './landing-layout.html',
   styleUrl: './landing-layout.css',
