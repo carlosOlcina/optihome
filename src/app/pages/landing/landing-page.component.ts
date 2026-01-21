080560889d36a18e82e0c699599a8bfa778ae7b2
carlosOlcina <carlosolcina23@gmail.com>
2026-01-21 08:43:05 +0100

    feat(landing): add benefits list and "Discover Products" button to `LandingMainSection`

--- src/app/pages/landing/landing-page.component.ts
+++ src/app/pages/landing/landing-page.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import {LandingMainSection} from '../../modules/landing/components/main-section/main-section';
+import {PrimaryButtonShared} from '../../share/components/buttons/primary-button/primary-button';
+import {Router} from '@angular/router';
+import {SizeEnum} from '../../config/enums/size-enum';
 
 @Component({
   selector: 'landing-page',
   imports: [
     LandingMainSection,
+    PrimaryButtonShared,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
 export class LandingPage {
+  constructor(private router: Router) {}
+
+  navigateToMain = ()=> {
+    void this.router.navigate(['/shop']);
+  }
+  protected readonly SizeEnum = SizeEnum;
 }
