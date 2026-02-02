07a0568406c9c0185d31c1c221de16cdc3aac028
carlosOlcina <carlosolcina23@gmail.com>
2026-02-02 20:50:57 +0100

    feat(landing): disable chevron buttons based on scroll position
    - Add logic to dynamically disable chevron buttons when at scroll boundaries.
    - Implement `disabledButton` signal to manage button states.
    - Update `ecosystem-section` template and component for improved usability.

--- src/app/components/landing/organism/ecosystem-section/ecosystem-section.ts
+++ src/app/components/landing/organism/ecosystem-section/ecosystem-section.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core'
+import { Component, ElementRef, signal, ViewChild, AfterViewInit } from '@angular/core'
 import { ProductCardLanding } from '../../molecules/product-card/product-card'
 import { SectionHeaderLanding } from '../../molecules/section-header/section-header'
 import ecosystemProducts from '../../../../data/landing/ecosystem-products'
@@ -10,17 +10,33 @@ import { ButtonChevron } from '../../../../share/components/buttons/button-chevr
   templateUrl: './ecosystem-section.html',
   styleUrl: './ecosystem-section.css',
 })
-export class EcosystemSection {
+export class EcosystemSection implements AfterViewInit {
   protected readonly ecosystemProducts = ecosystemProducts
 
   @ViewChild('scrollContainer', { read: ElementRef })
   private readonly scrollContainer?: ElementRef<HTMLElement>
+
+  readonly disabledButton = signal<Direction | null>(null)
+
+  syncButtons() {
+    const el = this.scrollContainer?.nativeElement
+    if (!el) return
+
+    const scrollWidth = el?.scrollWidth - window.innerWidth
+    if (el.scrollLeft < 100) this.disabledButton.set('left')
+    if (el.scrollLeft > scrollWidth - 100) this.disabledButton.set('right')
+  }
+
+  ngAfterViewInit(): void {
+    this.syncButtons()
+  }
 
   private scrollByAmount(amount: number) {
     const el = this.scrollContainer?.nativeElement
     if (!el) return
 
     el.scrollBy({ left: amount, behavior: 'smooth' })
+    this.syncButtons()
   }
 
   scrollLeft = () => this.scrollByAmount(-320)
