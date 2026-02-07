898e3b46ef9afee134c1effd7412d4fa2c2a393b
carlosOlcina <carlosolcina23@gmail.com>
2026-02-07 18:11:32 +0100

    style(landing): add waitlist component styles and structure
    - Introduce initial layout and styling for the waitlist section.
    - Implement responsive container design and input field focus states.
    - Add validation styles for invalid input fields.

---
+++ src/app/components/landing/pages/waitlist/waitlist.ts
@@ -0,0 +1,49 @@
+import { Component, signal } from '@angular/core'
+import { LandingCtaSection } from '../../organism/cta-section/cta-section'
+import { PrimaryButtonShared } from '../../../../share/components/buttons/primary-button/primary-button'
+import { SizeEnum } from '../../../../config/enums/size-enum'
+import { FormsModule } from '@angular/forms'
+import { email, form, FormField, required, submit } from '@angular/forms/signals'
+import { NgClass } from '@angular/common'
+
+interface SuscribeData {
+  email: string
+}
+
+@Component({
+  selector: 'landing-waitlist',
+  imports: [LandingCtaSection, PrimaryButtonShared, FormsModule, NgClass, FormField],
+  templateUrl: './waitlist.html',
+  styleUrl: './waitlist.css',
+})
+export class Waitlist {
+  protected readonly SizeEnum = SizeEnum
+
+  subscribers = signal(0)
+
+  suscribeModel = signal<SuscribeData>({
+    email: '',
+  })
+
+  suscribeForm = form(this.suscribeModel, (schemaPath) => {
+    required(schemaPath.email, { message: 'Es necesario introducir un emailo' })
+    email(schemaPath.email, { message: 'Es necesario introducir un email' })
+  })
+
+  onSubmit(event: Event) {
+    event.preventDefault()
+    submit(this.suscribeForm, async () => {
+      console.log(this.suscribeForm().invalid())
+      if (this.suscribeForm().invalid()) {
+        console.log('invalid')
+        return
+      } else {
+        this.subscribers.update((value: number) => value + 1)
+      }
+    }).then()
+  }
+
+  buttonAction() {
+    /* empty */
+  }
+}
