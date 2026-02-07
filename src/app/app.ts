88885e051557324bdff2c77f6ef9a2603652fae2
carlosOlcina <carlosolcina23@gmail.com>
2026-02-07 19:53:30 +0100

    feat(root): create notification molecule

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { Notifications } from './components/root/organism/notifications/notifications'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Notifications],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
