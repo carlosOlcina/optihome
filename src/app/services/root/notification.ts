587bd36fb5112df4ed96498a47f1f4664d91c17c
carlosOlcina <carlosolcina23@gmail.com>
2026-02-19 20:17:31 +0100

    docs(notification): add detailed documentation for NotificationService methods and properties

--- src/app/services/root/notification.ts
+++ src/app/services/root/notification.ts
@@ -1,17 +1,43 @@
 import { effect, Injectable, signal } from '@angular/core'
 
+/**
+ * El NotificationService gestiona una cola de notificaciones. Proporciona métodos para agregar,
+ * obtener y eliminar notificaciones, así como para eliminarlas automáticamente
+ * después de un tiempo determinado.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
   private notificationsQueue = signal<NotificationType[]>([])
 
+  /**
+   * Agrega una nueva notificación a la cola de notificaciones.
+   *
+   * @param {NotificationType} next - La notificación que se agregará a la cola.
+   * Actualiza la cola de notificaciones añadiendo la notificación proporcionada.
+   */
   setNotification = (next: NotificationType) => {
     this.notificationsQueue.update((notifications) => [...notifications, next])
   }
 
+  /**
+   * Un getter que proporciona acceso a la cola de notificaciones de manera de solo lectura.
+   *
+   * Esta variable expone los datos de las notificaciones como un flujo de solo lectura,
+   * asegurando que la cola de notificaciones subyacente no pueda ser modificada directamente
+   * por los consumidores.
+   *
+   * Utiliza esta propiedad para observar y manejar notificaciones sin alterar
+   * el estado original de la cola.
+   */
   getNotification = this.notificationsQueue.asReadonly()
 
+  /**
+   * Elimina una notificación de la cola de notificaciones en base al ID proporcionado.
+   *
+   * @param {string} id - El identificador único de la notificación que se desea eliminar.
+   */
   deleteNotification = (id: string) => {
     this.notificationsQueue.update((notifications) => [...notifications].filter((n) => n.id !== id))
   }
