f69a3c907ed1546211671c9e3cad2c6324b66f29
carlosOlcina <carlosolcina23@gmail.com>
2026-02-19 20:14:45 +0100

    docs(paragraph): add documentation for ParagraphLanding component

--- src/app/components/landing/atoms/paragraph/paragraph.ts
+++ src/app/components/landing/atoms/paragraph/paragraph.ts
@@ -1,6 +1,10 @@
 import { Component, input } from '@angular/core'
 import { NgStyle } from '@angular/common'
 
+/**
+ * Componente que representa un párrafo de la landing page.
+ * Permite personalizar la alineación del texto mediante la propiedad `centered`.
+ */
 @Component({
   selector: 'landing-paragraph',
   imports: [NgStyle],
@@ -8,5 +12,9 @@ import { NgStyle } from '@angular/common'
   styleUrl: './paragraph.css',
 })
 export class ParagraphLanding {
+  /**
+   * Centra el texto si es true.
+   * Por defecto: false
+   */
   centered = input<boolean>(false)
 }
