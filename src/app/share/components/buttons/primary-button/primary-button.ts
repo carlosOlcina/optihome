12bf280a49537bbd312afeb0378c0d279698aac2
carlosOlcina <carlosolcina23@gmail.com>
2026-02-20 19:52:32 +0100

    docs: enhance documentation for share components

--- src/app/share/components/buttons/primary-button/primary-button.ts
+++ src/app/share/components/buttons/primary-button/primary-button.ts
@@ -2,6 +2,12 @@ import { Component, input } from '@angular/core'
 import { SizeEnum } from '../../../../config/enums/size-enum'
 import { NgClass } from '@angular/common'
 
+/**
+ * Representa el componente de botón primario utilizado para acciones principales.
+ *
+ * Proporciona un botón con estilo y configurable que mantiene la
+ * consistencia visual para las interacciones principales en toda la aplicación.
+ */
 @Component({
   selector: 'ui-primary-button',
   imports: [NgClass],
@@ -9,6 +15,15 @@ import { NgClass } from '@angular/common'
   styleUrl: './primary-button.css',
 })
 export class PrimaryButtonShared {
+  /**
+   * Función obligatoria que se ejecuta cuando se hace clic en el botón.
+   */
   action = input.required<() => void>()
+
+  /**
+   * El tamaño del botón.
+   * Valores posibles: SizeEnum.small | SizeEnum.medium | SizeEnum.large
+   * Valor predeterminado: SizeEnum.medium
+   */
   size = input<SizeEnum>(SizeEnum.medium)
 }
